45d9217a860055f1ff994d49aec2cb6652c13a0b
Sachin Chuahan <csachinj@gmail.com>
2024-03-08 11:15:48 +0530

    #Update Learn

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TestService } from 'src/app/service/test.service';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 console.log('home component Loded....')
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  imports: [CommonModule,RouterOutlet, RouterLink, RouterLinkActive],
+  template: '<div class ="test"><p>Hello</p></div>',
+  styles: [`.test{font-size:20px;color:red}`]
 })
 export class HomeComponent implements OnInit {
 
