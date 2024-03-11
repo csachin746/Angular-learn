e40c1d60a0ab11d91c266b3bbf626a4cb57ba3b3
Sachin Chuahan <csachinj@gmail.com>
2024-03-11 11:04:43 +0530

    #Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './component/header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CardComponent } from './component/card/card.component';
+import { MaterialModule } from './module/material/material.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 console.log('Header Component Loded...')
 @Component({
-  // selector: 'app-header', // Selector As an Element
+  selector: 'app-header', // Selector As an Element
   // selector: '[app-header]', //Selector As an Attributes
   // selector: '.app-header' //Selector As a Class
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
-})
+  styleUrls: ['./header.component.scss'],
+  encapsulation:ViewEncapsulation.None})
 export class HeaderComponent {
 
 }
